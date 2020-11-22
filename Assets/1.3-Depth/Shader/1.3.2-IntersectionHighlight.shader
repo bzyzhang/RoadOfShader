89bee15cfcba594724276088829468c5dd6a2888
bzyzhang <longfeiah@gmail.com>
2020-11-22 17:11:37 +0800

    1.3.2-IntersectionHighlight---相交高亮

---
+++ Assets/1.3-Depth/Shader/1.3.2-IntersectionHighlight.shader
@@ -0,0 +1,96 @@
+﻿//https://www.jianshu.com/p/80a932d1f11e
+
+Shader "RoadOfShader/1.3-Depth/Intersection Highlight"
+{
+    Properties
+    {
+        _MainTex ("Texture", 2D) = "white" { }
+        _IntersectionColor ("Intersection Color", Color) = (1, 1, 0, 0)
+        _IntersectionWidth ("Intersection Width", Range(0, 1)) = 0.1
+    }
+    
+    SubShader
+    {
+        Tags { "Queue" = "Geometry" "RenderType" = "Opaque" "RenderPipeline" = "UniversalPipeline" "IgnoreProjector" = "True" }
+        
+        Pass
+        {
+            Tags { "LightMode" = "UniversalForward" }
+            
+            HLSLPROGRAM
+            
+            // Required to compile gles 2.0 with standard SRP library
+            // All shaders must be compiled with HLSLcc and currently only gles is not using HLSLcc by default
+            #pragma prefer_hlslcc gles
+            #pragma exclude_renderers d3d11_9x
+            #pragma target 2.0
+            
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
+            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
+            
+            #pragma vertex vert
+            #pragma fragment frag
+            
+            struct Attributes
+            {
+                float4 positionOS: POSITION;
+                float2 uv: TEXCOORD0;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+            };
+            
+            struct Varyings
+            {
+                float2 uv: TEXCOORD0;
+                float eyeZ: TEXCOORD1;
+                float4 positionScreen: TEXCOORD2;
+                float4 vertex: SV_POSITION;
+                UNITY_VERTEX_INPUT_INSTANCE_ID
+                UNITY_VERTEX_OUTPUT_STEREO
+            };
+            
+            CBUFFER_START(UnityPerMaterial)
+            float4 _MainTex_ST;
+            float4 _IntersectionColor;
+            float _IntersectionWidth;
+            CBUFFER_END
+            
+            TEXTURE2D(_MainTex);    SAMPLER(sampler_MainTex);
+            
+            Varyings vert(Attributes input)
+            {
+                Varyings output = (Varyings)0;
+                
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_TRANSFER_INSTANCE_ID(input, output);
+                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
+                
+                VertexPositionInputs vertexInput = GetVertexPositionInputs(input.positionOS.xyz);
+                output.vertex = vertexInput.positionCS;
+                output.uv = TRANSFORM_TEX(input.uv, _MainTex);
+                
+                output.positionScreen = vertexInput.positionNDC;
+                output.eyeZ = -vertexInput.positionVS.z;
+                
+                return output;
+            }
+            
+            half4 frag(Varyings input): SV_Target
+            {
+                UNITY_SETUP_INSTANCE_ID(input);
+                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+                
+                half4 color = SAMPLE_TEXTURE2D(_MainTex, sampler_MainTex, input.uv);
+                float screenZ = LinearEyeDepth(SampleSceneDepth(input.positionScreen.xy / input.positionScreen.w), _ZBufferParams);
+                
+                float halfWidth = _IntersectionWidth / 2;
+                float diff = saturate(abs(input.eyeZ - screenZ) / halfWidth);
+                
+                half4 finalColor = lerp(_IntersectionColor, color, diff);
+                
+                return finalColor;
+            }
+            ENDHLSL
+            
+        }
+    }
+}
---
+++ Assets/Editor/SceneHelper.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+[Serializable]
+public class SimpleObject
+{
+    public string Name;
+    public Vector3 Position;
+    public Vector3 EulerAngles;
+    public Vector3 Scale;
+}
+
+[Serializable]
+public class SimpleObjects
+{
+    public List<SimpleObject> Objects;
+}
+
+public class SceneHelper
+{
+    private static string FILE_NAME = "Objects.json";
+
+    [MenuItem("Tools/CollectObjects")]
+    private static void CollectObjects()
+    {
+        List<SimpleObject> objects = new List<SimpleObject>();
+
+        foreach (Transform transform in UnityEngine.Object.FindObjectsOfType(typeof(Transform)))
+        {
+            SimpleObject simpleObject = new SimpleObject
+            {
+                Name = transform.name,
+                Position = transform.position,
+                EulerAngles = transform.eulerAngles,
+                Scale = transform.localScale
+            };
+            objects.Add(simpleObject);
+        }
+
+        SimpleObjects SimpleObjects = new SimpleObjects
+        {
+            Objects = objects
+        };
+
+        var objectsJson = JsonUtility.ToJson(SimpleObjects,true);
+
+        var filePath = string.Format($"{Application.dataPath}/{FILE_NAME}");
+
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath);
+        }
+
+        File.WriteAllText(filePath,objectsJson);
+    }
+
+    [MenuItem("Tools/InstantiteObjects")]
+    private static void InstantiteObjects()
+    {
+        var filePath = string.Format($"{Application.dataPath}/{FILE_NAME}");
+
+        if (!File.Exists(filePath))
+        {
+            Debug.LogError("没有文件啊");
+            return;
+        }
+
+        var objectsStr = File.ReadAllText(filePath);
+        var objects = JsonUtility.FromJson<SimpleObjects>(objectsStr);
+
+        foreach (var item in objects.Objects)
+        {
+            if(item.Name.IndexOf("Main Camera") >= 0)
+            {
+                var camera = GameObject.Find("Main Camera");
+                if(camera)
+                {
+                    camera.transform.position = item.Position;
+                    camera.transform.eulerAngles = item.EulerAngles;
+                    camera.transform.localScale = item.Scale;
+                }
+            }
+            else if(item.Name.IndexOf("Directional Light") >= 0)
+            {
+                var light = GameObject.Find("Directional Light");
+                if (light)
+                {
+                    light.transform.position = item.Position;
+                    light.transform.eulerAngles = item.EulerAngles;
+                    light.transform.localScale = item.Scale;
+                }
+            }
+            else
+            {
+                var primitiveType = ConvertToPrimitiveType(item.Name);
+                var primitive = GameObject.CreatePrimitive(primitiveType);
+                primitive.name = item.Name;
+                primitive.transform.position = item.Position;
+                primitive.transform.eulerAngles = item.EulerAngles;
+                primitive.transform.localScale = item.Scale;
+            }
+        }
+    }
+
+    private static PrimitiveType ConvertToPrimitiveType(string name)
+    {
+        PrimitiveType primitiveType = PrimitiveType.Cube;
+
+        if(name.IndexOf("Sphere")  >= 0)
+        {
+            primitiveType = PrimitiveType.Sphere;
+        }
+        else if (name.IndexOf("Capsule") >= 0)
+        {
+            primitiveType = PrimitiveType.Capsule;
+        }
+        else if (name.IndexOf("Cylinder") >= 0)
+        {
+            primitiveType = PrimitiveType.Cylinder;
+        }
+        else if (name.IndexOf("Cube") >= 0)
+        {
+            primitiveType = PrimitiveType.Cube;
+        }
+        else if (name.IndexOf("Plane") >= 0)
+        {
+            primitiveType = PrimitiveType.Plane;
+        }
+        else if (name.IndexOf("Quad") >= 0)
+        {
+            primitiveType = PrimitiveType.Quad;
+        }
+        else
+        {
+            Debug.LogError(name);
+        }
+
+        return primitiveType;
+    }
+}
