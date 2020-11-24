917469cb965ed56ee4dc636b68605c357774397e
zhaolongfei <longfeiah@gmail.com>
2020-11-24 10:35:51 +0800

    1.3.5-ScanLine---扫描线

---
+++ Assets/1.3-Depth/Script/Overrides/ScanLine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace UnityEngine.Rendering.Universal
+{
+    [Serializable, VolumeComponentMenu("Custom Post-processing/Scan Line")]
+    public sealed class ScanLine : VolumeComponent, IPostProcessComponent
+    {
+        [Tooltip("是否开启效果")]
+        public BoolParameter enableEffect = new BoolParameter(false);
+        [Tooltip("扫描线颜色")]
+        public ColorParameter LineColor = new ColorParameter(new Color(0, 0.8f, 0.2f, 1), false, false, true);
+        [Tooltip("扫描线宽度")]
+        public ClampedFloatParameter LineWidth = new ClampedFloatParameter(0.05f, 0, 0.08f);
+        [Tooltip("扫描线深度")]
+        public ClampedFloatParameter CurValue = new ClampedFloatParameter(0, 0, 0.9f);
+
+        public bool IsActive() => enableEffect == true;
+
+        public bool IsTileCompatible() => false;
+    }
+}
---
+++ Assets/1.3-Depth/Script/Pass/ScanLinePass.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+namespace UnityEngine.Experiemntal.Rendering.Universal
+{
+    public class ScanLinePass : ScriptableRenderPass
+    {
+        static readonly string k_RenderTag = "Scan Line";
+
+        private ScanLine scanLine;
+        private Material scanLineMat;
+
+        RenderTargetIdentifier currentTarget;
+        private RenderTargetHandle destination { get; set; }
+
+        public ScanLinePass()
+        {
+            var shader = Shader.Find("RoadOfShader/1.3-Depth/Scan Line");
+            scanLineMat = CoreUtils.CreateEngineMaterial(shader);
+        }
+
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            if (scanLineMat == null)
+            {
+                UnityEngine.Debug.LogError("材质没找到！");
+                return;
+            }
+            if (!renderingData.cameraData.postProcessEnabled) return;
+            //通过队列来找到HologramBlock组件，然后
+            var stack = VolumeManager.instance.stack;
+            scanLine = stack.GetComponent<ScanLine>();
+            if (scanLine == null) { return; }
+            if (!scanLine.IsActive()) return;
+
+            var cmd = CommandBufferPool.Get(k_RenderTag);
+            Render(cmd, ref renderingData);
+            context.ExecuteCommandBuffer(cmd);
+            CommandBufferPool.Release(cmd);
+        }
+
+        void Render(CommandBuffer cmd, ref RenderingData renderingData)
+        {
+            if (renderingData.cameraData.isSceneViewCamera) return;
+
+            var lineColor = scanLine.LineColor.value.linear;
+            var lineWidth = scanLine.LineWidth.value;
+            var curValue = scanLine.CurValue.value;
+            scanLineMat.SetColor("_LineColor", lineColor);
+            scanLineMat.SetFloat("_LineWidth", lineWidth);
+            scanLineMat.SetFloat("_CurValue", curValue);
+
+            var source = currentTarget;
+
+            Blit(cmd, source, destination.Identifier(), scanLineMat);
+        }
+
+        public void Setup(in RenderTargetIdentifier currentTarget, RenderTargetHandle dest)
+        {
+            this.destination = dest;
+            this.currentTarget = currentTarget;
+        }
+    }
+}
